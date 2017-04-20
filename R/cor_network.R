e5c23d69f40e41226f255b0562cce84a3929cbfd
julieklessner <jktp11@student.aau.dk>
2017-04-20 12:44:15 +0200

    Adding correlation methods, defining output
    Adding Spearman, SparCC, CClasso and REBACCA as methods for calculating
    correlation matrices.
    
    Defining outputs.

--- R/cor_network.R
+++ R/cor_network.R
@@ -3,10 +3,12 @@
 #' This function allows you to build a network based on correlations
 #' @param data (required) A phyloseq object.
 #' @param threshold.cor Threshold value for the correlation (default: 0.8).
-#' @param threshold.count Threshold value for abundance reads (default: 10).
+#' @param threshold.abund Threshold value for relative abundance (default: 0.01).
+#' @param threshold.obs Threshold value for the average relative abundance per observation (default: 0.1).
+#' @param threshold.pval Threshold value for the p-value (default: 0.05).
 #' @param show.label Show vertex names as labels (default: TRUE).
 #' @param scale.abund Scale the node size according to average abundance (default: FALSE).
-#' @param node.size Size of nodes if scale.abund=FALSE (default: 5) 
+#' @param node.size Size of nodes if scale.abund=FALSE (default: 5)
 #' @param scale.cor Scale the edge color according to strength of correlation (default: FALSE).
 #' @param edge.size Size of egdes if scale.cor=FALSE (default: 0.5)
 #' @param tax.aggregate The taxonomic level that the data should be aggregated to (default: "Phylum")
@@ -18,106 +20,209 @@
 #' @param highlight.label Highlight OTUs using node labels, e.g. c("MiDAS_37", "MiDAS_73") (default:none).
 #' @param add.tax.info Show taxonomic information (default: FALSE).
 #' @param show.edge.label Show correlation values as edge labels (default: FALSE).
+#' @param correlation Choose the method for calculation of correlation from Pearson, Spearman, SparCC, CClasso or Rebacca (default: "Pearson").
+#' @param show.top Number of correlations returned if return.output is set to top (default: 25).
+#' @param return.output Either "top" or "total" or "plot" (default: "top").
 #' @keywords network correlation ggnetwork
 #' @import network
 #' @import ggnetwork
 #' @import sna
+#' @import viridis
+#' @import MCMCpack
+#' @import glmnet
+#' @import parallel
 #' @export
 
-cor_network <- function(data, threshold.cor= 0.8, threshold.count=10, show.label=TRUE, scale.abund=FALSE, node.size=5, scale.cor=FALSE, edge.size = 0.5, highlight.OTU=NULL, highlight.color=NULL, highlight.size=10, highlight.label=NULL, tax.aggregate="Phylum", tax.add=NULL, tax.class=NULL, tax.empty="best", add.tax.info=FALSE, show.edge.label=FALSE){
-  
+cor_network <- function(data, threshold.cor= 0.8, threshold.abund=0.01, threshold.obs=0.1, threshold.pval=0.05, show.label=TRUE, scale.abund=FALSE, node.size=5, scale.cor=FALSE, edge.size = 0.5, highlight.OTU=NULL, highlight.color=NULL, highlight.size=10, highlight.label=NULL, tax.aggregate="Phylum", tax.add=NULL, tax.class=NULL, tax.empty="best", add.tax.info=FALSE, show.edge.label=FALSE, correlation = "Pearson", return.output = "top", show.top = 25){
+
   data0 <- list(abund = as.data.frame(otu_table(data)@.Data),
                 tax = data.frame(tax_table(data)@.Data, OTU =   rownames(tax_table(data))),
                 sample = suppressWarnings(as.data.frame(as.matrix(sample_data(data)))))
-  
+
   # Clean and rename taxonomy -------------------------------------------------------
-  
+
   data1 <- amp_rename(data = data0,
-                      tax.class=tax.class, 
-                      tax.empty=tax.empty, 
+                      tax.class=tax.class,
+                      tax.empty=tax.empty,
                       tax.level = tax.aggregate)
-  
+
   # Divide data to seperate data frames --------------------------------------------
-  
+
   abund <- data1[["abund"]]
   tax <- data1[["tax"]]
   sample <- data1[["sample"]]
-  
-  # Subsetting by threshold.count-----------------------------------------------------  
-  
-  abund[abund <= threshold.count] <- NA
-  
-  # Producing correlation matrix-----------------------------------------------------
-  
-  cormat <- cor(t(abund), method = "pearson")
-  
-  # Revoming self-correlation-------------------------------------------------------  
-  
-  diag(cormat) <- NA  
-  
+
+  # Normalising abundance
+  abund_n <- apply(as.matrix(abund), 2, function(x) x / sum(x) * 100) %>%  as.data.frame()
+
+  # Subsetting by threshold.obs------------------------------------------------
+  abund_n1 <- abund_n[rowMeans(abund_n) >= threshold.obs,]
+
+  # Subsetting by threshold.abund
+  abund_n1[abund_n1 <= threshold.abund] <- 0
+
+  # Subsetting abundance count
+  otus <- rownames(abund_n1)
+  abunds <- abund[otus, ]
+
+  ##################################
+  # Producing correlation matrix   #
+  ##################################
+
+
+  if(correlation == "Pearson"){
+    resp <- rcorr(as.matrix(t(abunds)), type = "pearson")
+    cormat1 <- resp$r %>% as.matrix()
+    pmat <- resp$P %>% as.matrix()
+    cormat <- ifelse(pmat <= threshold.pval, cormat1, NA)
+  }
+
+  if(correlation == "Spearman"){
+    resp <- rcorr(as.matrix(t(abunds)), type = "spearman")
+    cormat1 <- resp$r %>% as.matrix()
+    pmat <- resp$P %>% as.matrix()
+    cormat <- ifelse(pmat <= threshold.pval, cormat1, NA)
+  }
+
+  if(correlation == "SparCC"){
+    res_cc <- sparcc(t(abunds))
+    cormat1 <- res_cc$Cor
+    pmat <- cormat1
+    dfr = nrow(t(abunds))-2
+    above <- row(pmat) < col(pmat)
+    r2 <- pmat[above]^2
+    Fstat <- r2 * dfr/(1 - r2)
+
+    pmat[above] <- 1 - pf(Fstat, 1, dfr)
+    pmat[row(pmat) == col(pmat)] <- NA
+
+    rownames(cormat1) <- rownames(abunds)
+    colnames(cormat1) <- rownames(abunds)
+    rownames(pmat) <- rownames(abunds)
+    colnames(pmat) <- rownames(abunds)
+    cormat <- ifelse(pmat <= threshold.pval, cormat1, NA)
+  }
+
+  if(correlation == "CClasso"){
+    res_cc <- cclasso(t(abunds), counts = TRUE, n_boot = 20)
+    cormat1 <- res_cc$cor_w %>% as.matrix()
+    rownames(cormat1) <- rownames(abunds)
+    colnames(cormat1) <- rownames(abunds)
+    pmat <- res_cc$p_vals %>% as.matrix
+    rownames(pmat) <- rownames(abunds)
+    colnames(pmat) <- rownames(abunds)
+    cormat <- ifelse(pmat <= threshold.pval, cormat1, NA)
+  }
+
+  if(correlation == "Rebacca"){
+    x.rslt <- rebacca(as.matrix(abunds), nbootstrap=20, N.cores=1)
+    tau = stability_cutoff(x.rslt$Stability, x.rslt$q, B=50, FWER=threshold.pval)
+    x.adj = sscore2adjmatrix(x.rslt$Stability, tau)
+    x.est = rebacca_adjm2corr(abunds, x.adj)
+
+    cormat <- x.est$corr %>% as.matrix()
+    rownames(cormat) <- rownames(abunds)
+    colnames(cormat) <- rownames(abunds)
+  }
+
+  # Revoming self-correlation-------------------------------------------------------
+
+  diag(cormat) <- NA
+
   # Removing "doubles"---------------------------------------------------------------
-  
+
   cormat[lower.tri(cormat)] <- NA
-  
+
   # Converting to dataframe for easier subsetting------------------------------------
-  
-  cor2 <- melt(cormat, value.name="Rval")
-  cor3 <- cor2[!is.na(cor2$Rval), ]
-  
+
+  cor2 <- melt(cormat, value.name="Cor_val")
+  cor3 <- cor2[!is.na(cor2$Cor_val), ]
+
   # Subsetting by threshold.cor------------------------------------------------------
-  
-  cor4 <- subset(cor3, abs(cor3$Rval) >= threshold.cor)
-  
-  #Building network ----------------------------------------------------------------
-  
+
+  cor4 <- subset(cor3, abs(cor3$Cor_val) >= threshold.cor)
+
+  ####################
+  # Building network #
+  ####################
+
   net <- network(cor4[ ,1:2 ], directed=FALSE)
-  
-  #Assigning Rval to edges
-  
-  set.edge.attribute(net, "Rval", cor4$Rval)
-  
-  #Assigning abundance values to nodes---------------------------------------------
-  
+
+  # Assigning correlation values to edges ---------------------------------------------
+
+  set.edge.attribute(net, "Cor_val", cor4$Cor_val)
+
+  ##################################################
+  # Assigning normalised abundance values to nodes #
+  ##################################################
+
   names = ggnetwork(net)$vertex.names %>% unique() %>% as.character()
   names2 = ggnetwork(net)$vertex.names %>% as.data.frame()
-  
-  abund_cor <- abund[names, ] 
-  abund_mean <- rowMeans(abund_cor) 
-  abund_mean2 <- abund_mean %>% as.data.frame()
-  
+
+
+  ## Matching abundances to correlated abundance------------------------------------
+  abund_cor <- abund_n[names, ]
+
+  ## Calculating mean abundance -------------------------------------------------------
+
+  abund_mean <- rowMeans(abund_cor)
+  #abund_mean2 <- abund_mean %>% as.data.frame()
+
+  ## Asigning abundance values to vertices --------------------------------------------
   net %v% "Avg_Abundance" <- abund_mean
-  
-  #Assigning taxonomic information to nodes
-  
+
+  ############################################
+  # Assigning taxonomic information to nodes #
+  ############################################
+
   tax <- data.frame(tax, Display = tax[,tax.aggregate])
   rownames(tax) <- tax$OTU
-  
-  tax_info = tax[names, ] 
+
+  tax_info = tax[names, ]
   tax_info_display = tax_info[,"Display"] %>% as.character()
-  
+  tax_info_display2 = tax_info[,"Display"] %>% as.data.frame()
+
   set.vertex.attribute(net, "taxo_info", tax_info_display)
-  
-  #Visualizing using ggnetwork-------------------------------------------------------
-  
-  
+
+  ################################
+  # Visualizing using ggnetwork  #
+  ################################
+
   p <- ggplot(net, arrow.gap = 0, aes(x=x, y=y, xend=xend, yend=yend)) +
     geom_nodes()+
     theme_blank()
-  
-  #Options for edges---------------------------------------------------------------- 
+
+  # Options for edges----------------------------------------------------------------
   if (scale.cor == T){
-    p <- p + geom_edges(alpha = 0.6, size=2, aes(color = Rval)) #+ scale_color_gradient2()
+    p <- p + geom_edges(alpha = 0.6, size=2, aes(color = Cor_val)) +
+      scale_color_viridis("Correlation value") #+ scale_color_gradient2()
   } else {
     p <- p + geom_edges(alpha = 0.6, size=edge.size)
   }
-  
-  #Options for nodes----------------------------------------------------------------
+
+  # Options for highlights------------------------------------------------------------
+
+  if(!is.null(highlight.OTU)){
+    p <- p + geom_nodes(data = function(x) { x[ x$vertex.names %in% highlight.OTU, ]},
+                        col=highlight.color,
+                        size= highlight.size,
+                        alpha = 0.2)
+  }
+
+  if(!is.null(highlight.label)){
+    p <- p + geom_nodelabel_repel(aes(label = vertex.names),
+                                  box.padding = unit(1, "lines"),
+                                  data = function(x) { x[ x$vertex.names %in% highlight.label, ]})
+  }
+
+  # Options for nodes----------------------------------------------------------------
   if (scale.abund == T){
-    p <- p + geom_nodes(aes(size= Avg_Abundance))
+    p <- p + geom_nodes(aes(size= Avg_Abundance)) +
+      scale_size(name="Mean % Read Abundance", range=c(node.size, (node.size+4)))
   } else {
     p <- p + geom_nodes(size = node.size)
   }
-  
+
   if(add.tax.info == T & scale.cor == T){
     print("Mapping a color to both a vertex attribute and an edge attribute will violate the grammer of graphics")
   } else if (add.tax.info == T & scale.abund == T) {
@@ -125,26 +230,59 @@ cor_network <- function(data, threshold.cor= 0.8, threshold.count=10, show.label
   } else if (add.tax.info == T & scale.abund == F) {
     p <- p + geom_nodes(aes(size= node.size, col=taxo_info))
   }
-  
-  #Options for labels----------------------------------------------------------------
+
+  # Options for labels----------------------------------------------------------------
   if(show.edge.label == T){
     p <- p + geom_edgelabel(aes(label = signif(Rval, digits = 2)))
   }
-  
+
   if (show.label == T){
-    p <- p + geom_nodetext(aes(label=vertex.names), nudge_x = 0.05) 
+    p <- p + geom_nodetext(aes(label=vertex.names), nudge_x = 0.05)
   }
-  
-  #Options for highlights------------------------------------------------------------
-  
-  if(!is.null(highlight.OTU)){
-    p <- p + geom_nodes(data = function(x) { x[ x$vertex.names == highlight.OTU, ]}, col=highlight.color, size= 10, alpha = 0.2)
+
+
+  ##########################################
+  # Matching taxonomy to correlation pairs #
+  ##########################################
+
+  #Retrieving taxonomic information for Var2
+  Var2 <- cor4[, "Var2", drop=FALSE]
+  Tax_Var2 <- tax[rownames(tax) %in% cor4$Var2,]
+  Tax_Var2 <- Tax_Var2[ , c("OTU", "Display")]
+  cor5 <- merge(cor4, Tax_Var2, by.x=2, by.y=1)
+
+  #Retrieving taxonomic information for Var1
+  Var1 <- cor4[, "Var1", drop=FALSE]
+  Tax_Var1 <- tax[rownames(tax) %in% cor4$Var1,]
+  Tax_Var1 <- Tax_Var1[ , c("OTU", "Display")]
+  cor5 <- merge(cor5, Tax_Var1, by.x=2, by.y=1)
+
+  ###################
+  # Defining Output #
+  ###################
+
+  #cor4$Rel_abund <- abund_n
+  Result_list_total <- data.frame(
+    Cor1 = cor5$Var1,
+    Tax1 = cor5$Display.y,
+    Cor2 = cor5$Var2,
+    Tax2 = cor5$Display.x,
+    Cor_val = cor5$Cor_val)
+
+  if(return.output == "total"){
+    outlist <- list(network = p, Cor_result = Result_list_total)
+    return(outlist)
   }
-  
-  if(!is.null(highlight.label)){
-    p <- p + geom_nodelabel_repel(aes(label = vertex.names),
-                                  box.padding = unit(1, "lines"),
-                                  data = function(x) { x[ x$vertex.names == highlight.label, ]})
-  }  
-p
+
+  if(return.output == "top"){
+    Result_list_top <- arrange(Result_list_total, desc(Cor_val)) %>% head(n=show.top)
+    outlist <- list(network = p, Cor_result = Result_list_top)
+    return(outlist)
+  }
+
+if(return.output == "plot"){
+  return(p)
+}
+
+
   }
